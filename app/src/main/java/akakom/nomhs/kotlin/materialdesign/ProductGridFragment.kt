f8e838cf0d9ec0dcdebcf30aae468c613846b092
aisyahfrds <aisyahfirdausi73@gmail.com>
2020-05-05 11:02:48 +0700

    commit

--- app/src/main/java/akakom/nomhs/kotlin/materialdesign/ProductGridFragment.kt
+++ app/src/main/java/akakom/nomhs/kotlin/materialdesign/ProductGridFragment.kt
@@ -2,9 +2,12 @@ package akakom.nomhs.kotlin.materialdesign
 
 import akakom.nomhs.kotlin.materialdesign.network.ProductEntry
 import akakom.nomhs.kotlin.materialdesign.staggeredgridlayout.StaggeredProductCardRecyclerViewAdapter
+import android.os.Build
 import android.os.Bundle
 import android.view.*
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -24,6 +27,12 @@ class ProductGridFragment : Fragment() {
 
         // Set up the toolbar.
         (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
+        view.app_bar.setNavigationOnClickListener(NavigationIconClickListener(
+            activity!!,
+            view.product_grid,
+            AccelerateDecelerateInterpolator(),
+            ContextCompat.getDrawable(context!!, R.drawable.shr_branded_menu), // Menu open icon
+            ContextCompat.getDrawable(context!!, R.drawable.shr_close_menu))) // Menu close icon
 
         // Set up the RecyclerView
         view.recycler_view.setHasFixedSize(true)
@@ -33,6 +42,12 @@ class ProductGridFragment : Fragment() {
                 return if (position % 3 == 2) 2 else 1
             }
         }
+
+        // Set cut corner background for API 23+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            view.product_grid.background = context?.getDrawable(R.drawable.shr_product_grid_background_shape)
+        }
+
         view.recycler_view.layoutManager = gridLayoutManager
         val adapter = StaggeredProductCardRecyclerViewAdapter(
             ProductEntry.initProductEntryList(resources))
@@ -40,6 +55,7 @@ class ProductGridFragment : Fragment() {
         val largePadding = resources.getDimensionPixelSize(R.dimen.shr_staggered_product_grid_spacing_large)
         val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_staggered_product_grid_spacing_small)
         view.recycler_view.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+
 
 
 
